8e535a5badb3668f0449f4c9e909bd7b430ab3f8
NajlaHanifahMuhtadi <najlahanifahm@gmail.com>
2026-05-28 11:34:06 +0700

    menambahkan exception handling

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -163,5 +163,10 @@ int main()
                 }
             }
         }
+        catch (exception &e)
+        {
+            cout << "Check for the values entered." << endl;
+        }
     }
+    return 0;
 }
